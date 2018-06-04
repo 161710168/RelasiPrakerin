b5f1eec1249e236c6adc7adbbf87a6576fc234c5
Yuda Dermawan <33020930+161710168@users.noreply.github.com>
2018-06-04 04:50:50 +0300

    Update Siswa.php

--- app/Siswa.php
+++ app/Siswa.php
@@ -10,12 +10,7 @@ class Siswa extends Model
     protected $fillable=['id_kelas','nama_siswa','nis','alamat','no_hp'];
     protected $visible=['id_kelas','nama_siswa','nis','alamat','no_hp'];
     public $timestamps=true;
-
-    public function orangtua()
-    {
-    	return $this->belongsTo('App\OrangTua','id_ortu');
-    }
-
+    
     public function kelas()
     {
     	return $this->belongsTo('App\kelas','id_kelas');
