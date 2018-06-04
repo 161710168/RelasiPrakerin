470da8cc4a9ca22cb0acdf6035d1dc3b87828bc3
Yuda Dermawan <33020930+161710168@users.noreply.github.com>
2018-06-04 04:46:41 +0300

    Update Siswa.php

--- app/Siswa.php
+++ app/Siswa.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Siswa extends Model
 {
     protected $table = 'siswas';
-    protected $fillable=['id_kelas','id_ortu','nama_siswa','nis','alamat','no_hp'];
-    protected $visible=['id_kelas','id_ortu','nama_siswa','nis','alamat','no_hp'];
+    protected $fillable=['id_kelas','nama_siswa','nis','alamat','no_hp'];
+    protected $visible=['id_kelas','nama_siswa','nis','alamat','no_hp'];
     public $timestamps=true;
 
     public function orangtua()
