60a038bcdb0b60a659be95e1e90458383be6e6f8
Yuda Dermawan <33020930+161710168@users.noreply.github.com>
2018-06-04 04:16:47 +0300

    Update 2017_12_22_024529_create_siswas_table.php

--- database/migrations/2017_12_22_024529_create_siswas_table.php
+++ database/migrations/2017_12_22_024529_create_siswas_table.php
@@ -16,7 +16,6 @@ class CreateSiswasTable extends Migration
         Schema::create('siswas', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('id_kelas')->unsigned();
-            $table->integer('id_ortu')->unsigned();
             $table->string('nama_siswa');
             $table->string('nis');
             $table->text('alamat');
