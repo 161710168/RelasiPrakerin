7b06f581043eb9411e07df9f2ceaf94d96cc4148
Yuda Dermawan <33020930+161710168@users.noreply.github.com>
2018-06-04 04:50:21 +0300

    Update Absensi.php

--- app/Absensi.php
+++ app/Absensi.php
@@ -6,17 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Absensi extends Model
 {
-    protected $fillable = ['id_siswa','keterangan','tgl','id_kelas','id_ortu'];
+    protected $fillable = ['id_siswa','keterangan','tgl','id_kelas'];
 
     public function siswa()
     {
     	return $this->belongsTo('App\siswa','id_siswa');
     }
-
- 	public function orangtua()
-     {
-     	return $this->belongsTo('App\orangtua', 'id_ortu');
-     }
     public function kelas()
      {
      	return $this->belongsTo('App\Kelas', 'id_kelas');
